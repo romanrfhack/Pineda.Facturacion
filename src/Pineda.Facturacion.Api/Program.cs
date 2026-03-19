64e421be907af540def3531a2aaf082d12e63ffe
Román Romero Figueroa <romanrfhack@gmail.com>
2026-03-19 08:26:52 -0600

    feat: wire application and infrastructure in api

--- src/Pineda.Facturacion.Api/Program.cs
+++ src/Pineda.Facturacion.Api/Program.cs
@@ -1,12 +1,18 @@
+using Pineda.Facturacion.Application.DependencyInjection;
+using Pineda.Facturacion.Infrastructure.DependencyInjection;
+using Pineda.Facturacion.Infrastructure.BillingWrite.DependencyInjection;
+using Pineda.Facturacion.Infrastructure.LegacyRead.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+builder.Services.AddLegacyReadInfrastructure(builder.Configuration);
+builder.Services.AddBillingWriteInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +20,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
