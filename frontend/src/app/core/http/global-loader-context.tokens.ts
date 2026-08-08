fb57b035e3019e448a8bf08707ca6426376c5d9c
romanrfhack <romanrfhack@gmail.com>
2026-08-08 07:34:15 -0600

    feat: add helper for selective GET loader contexts

--- frontend/src/app/core/http/global-loader-context.tokens.ts
+++ frontend/src/app/core/http/global-loader-context.tokens.ts
@@ -1,4 +1,4 @@
-import { HttpContextToken } from '@angular/common/http';
+import { HttpContext, HttpContextToken } from '@angular/common/http';
 import { GlobalLoaderOptions } from '../ui/global-loader.service';
 
 export interface GlobalHttpLoaderOptions extends GlobalLoaderOptions {
@@ -9,3 +9,7 @@ export interface GlobalHttpLoaderOptions extends GlobalLoaderOptions {
 export const SKIP_GLOBAL_LOADER = new HttpContextToken<boolean>(() => false);
 
 export const GLOBAL_LOADER_OPTIONS = new HttpContextToken<GlobalHttpLoaderOptions | null>(() => null);
+
+export function createGlobalLoaderContext(options: GlobalHttpLoaderOptions): HttpContext {
+  return new HttpContext().set(GLOBAL_LOADER_OPTIONS, options);
+}
